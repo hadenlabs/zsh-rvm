fe2c056d856c1fbcae5372ce3c32f665e6b1c72c
@slovacus <slovacus@gmail.com>
2019-11-18 12:40:14 -0500

    refactor: code for install zsh-rvm (#7)

--- zsh-rvm.zsh
+++ zsh-rvm.zsh
@@ -8,35 +8,47 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-LIGHT_GREEN='\033[1;32m'
-GREEN='\033[0;32m'
-CLEAR='\033[0m'
+PACKAGE_NAME=rvm
+
+plugin_dir=$(dirname "${0}":A)
+# shellcheck source=/dev/null
+source "${plugin_dir}"/src/helpers/messages.zsh
 
 function rvm::install {
-    echo -e "${CLEAR}${LIGHT_GREEN}Installing RVM${CLEAR}"
+    message_info "Installing ${PACKAGE_NAME}"
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     curl -sSL https://get.rvm.io | bash -s stable
     rvm::custom
+    message_success "Installed ${PACKAGE_NAME}"
 }
 
 function rvm::custom {
-    if (( $+commands[rvm] )); then
-        echo -e "${CLEAR}${LIGHT_GREEN} Install Ruby ${CLEAR}"
+    if [[ -x "$(command which rvm)" ]]; then
+        message_info "Installing ruby ${PACKAGE_NAME}"
         rvm install 2.5.3
         rvm use 2.5.3 --default
-        echo -e "${CLEAR}${LIGHT_GREEN}Installing required packages${CLEAR}"
-        gem install tmuxinator
+        message_success "Installed ${PACKAGE_NAME}"
+    fi
+}
+
+function rvm::post_install {
+    if [[ -x "$(command which ruby)" ]]; then
+        message_info "Installing gems ${PACKAGE_NAME}"
+        gem install tmuxinator \
+            cocoapods
+        message_success "Installed ${PACKAGE_NAME}"
     fi
 }
 
 function rvm::init {
     # Add RVM to PATH for scripting
-    [[ -e "$HOME/.rvm/bin" ]] && export PATH="$PATH:$HOME/.rvm/bin"
-    [[ -e "/usr/local/rvm/bin" ]] && export PATH="$PATH:/usr/local/rvm/bin"
+    [[ -e "${HOME}/.rvm/bin" ]] && export PATH="${PATH}:${HOME}/.rvm/bin"
+    [[ -e "/usr/local/rvm/bin" ]] && export PATH="${PATH}:/usr/local/rvm/bin"
 }
 
 rvm::init
 
-if (( ! $+commands[rvm] )); then
+if [[ ! -x "$(command which rvm)" ]]; then
     rvm::install
+    rvm::post_install
 fi
