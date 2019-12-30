d80887fb0c3a4969c8bcd27e32437e7e93aaaa70
Luis Mayta <luis@hadenlabs.com>
2019-12-29 20:19:13 -0500

    fix: name package for message (#19)

--- zsh-rvm.zsh
+++ zsh-rvm.zsh
@@ -8,42 +8,43 @@
 #   Luis Mayta <slovacus@gmail.com>
 #
 
-package_name=rvm
+rvm_package_name=rvm
 
 function rvm::install {
-    message_info "Installing ${package_name}"
+    message_info "Installing ${rvm_package_name}"
 
-    if [ -x "$(command which gpg)" ]; then
+    if type -p gpg > /dev/null; then
         gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     fi
 
-    if [ -x "$(command which gpg2)" ]; then
+    if type -p gpg2 > /dev/null; then
         gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
     fi
 
     curl -sSL https://get.rvm.io | bash -s stable
 
     rvm::custom
-    message_success "Installed ${package_name}"
+    message_success "Installed ${rvm_package_name}"
+    rvm::post_install
 }
 
 function rvm::custom {
     if [ -e "${HOME}/.rvm" ]; then
         rmv::init
-        message_info "Installing ruby ${package_name}"
+        message_info "Installing ruby ${rvm_package_name}"
         rvm install 2.5.3
         rvm use 2.5.3 --default
-        message_success "Installed ${package_name}"
+        message_success "Installed ${rvm_package_name}"
     fi
 }
 
 function rvm::post_install {
-    if [ -x "$(command which ruby)" ]; then
-        message_info "Installing gems ${package_name}"
+    if type -p ruby > /dev/null; then
+        message_info "Installing gems ${rvm_package_name}"
         gem install tmuxinator \
             cocoapods \
             terminal-notifier
-        message_success "Installed ${package_name}"
+        message_success "Installed ${rvm_package_name}"
     fi
 }
 
@@ -57,5 +58,4 @@ rvm::init
 
 if [ ! -e "${HOME}/.rvm" ]; then
     rvm::install
-    rvm::post_install
 fi
