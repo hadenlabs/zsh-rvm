aac4e4c6a13e42527ccd6d6a68382e8b3d217a9a
Luis Mayta <luis@hadenlabs.com>
2024-11-10 18:39:16 -0300

    feat(core): implement function for install rvm (#57)

--- config/base.zsh
+++ config/base.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
-export RVM_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+export RVM_MESSAGE_BREW="Please install brew or use antibody bundle hadenlabs/zsh-brew"
 export RVM_PACKAGE_NAME=rvm
 export RVM_ROOT="${HOME}"/.rvm
 export RVM_CACHE_DIR="${HOME}/.cache/rvm"
